7f93d462e6187a27adc59f69088e63642867ac6e
Your Name <you@example.com>
2018-05-19 14:57:07 +0800

    用户注册

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/123';
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
